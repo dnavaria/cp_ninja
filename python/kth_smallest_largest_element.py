a94569225e2bfa7d2dcadae60abfaeb6f18f463c
dnavaria <dk17816@gmail.com>
2022-02-01 09:59:45 +0530

    kth smallest and largest

--- python/kth_smallest_largest_element.py
+++ python/kth_smallest_largest_element.py
@@ -1,7 +1,37 @@
-import heapq
+import sys
+def kths(arr,k):
+    s = 0
+    mn = -1
+    c=0
+    while s <len(arr):
+        if arr[s] > mn:
+            mn = arr[s]
+            c+=1
+        if c == k:
+            return arr[s]
+        s+=1
+    return -1
 
-def kth(arr,k):
-    heapq.nlargest(arr)
+def kthl(arr,k):
+    e = len(arr)-1
+    mn = sys.maxsize
+    c=0
+    while e >=0:
+        if arr[e] < mn:
+            mn = arr[e]
+            c+=1
+        if c == k:
+            return arr[e]
+        e-=1
+    return -1
+
+
+def kthSmallestLargest(arr,k):
+    arr.sort()
+    large = kthl(arr,k)
+    small = kths(arr,k)
+    
+    return small,large
     
 
 n = int(input())
