3c1a7586989211588aefa2161a589c5c0734ab13
dnavaria <dk17816@gmail.com>
2022-02-13 12:43:56 +0530

    sum of infinite array optimal

--- python/sum_of_infinite_array.py
+++ python/sum_of_infinite_array.py
@@ -1,17 +1,25 @@
+mod = 10**9+7
+
+def helper(pref, x, n):
+    count = (x // n) % mod
+    res = (pref[n] * count) % mod
+    return (res + pref[x%n]) % mod
+
 def sumInRanges(arr, n, queries, q):
-    mx = -1
-    for i in queries:
-        if i[1] > mx:
-            mx = i[1]
-    pref = [0]*len(arr)
-    for i in range(len(arr)):
-        pref[i] = pref[i-1] + arr[i]
-    iter = 0
     ans = []
-    while q > 0:
-        l,r = queries[iter]
-        ans.append(pref[r] - pref[l-1])
-        iter+=1
-        q-=1
-    return ans
+    # creating an empty prefix array of length n+1
+    pref = [0 for i in range(n+1)]
     
+    # populating the prefix array with the cummulative sum
+    for i in range(1,n+1):
+        pref[i] = (pref[i-1] + arr[i-1]) % mod
+        
+    for l,r in queries:
+        # calculating rsum
+        rsum = helper(pref,r,n)
+        # calculating lsum
+        lsum = helper(pref,l-1,n)
+        
+        ans.append((rsum - lsum)%mod)
+    
+    return ans
