df92216b7484ce9ae0dedac6e191bdc798776200
dnavaria <dk17816@gmail.com>
2022-02-20 11:46:44 +0530

    find lone set bit

--- python/find_lone_set_bit.py
+++ python/find_lone_set_bit.py
@@ -1,9 +1,9 @@
 def findSetBit(N):
-    if N and (N & (N-1)):
-        cnt = 1
-        while N > 0:
-            if N & 1:
-                return cnt
-            cnt+=1
-            N>>=1
-    return -1
+	if N and (not(N & (N-1))):
+		cnt = 1
+		while N > 0:
+			if N >> 1 == 0:
+				return cnt
+			cnt+=1
+			N>>=1
+	return -1
