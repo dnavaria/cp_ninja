2840415f88d93bbc6d421866f80c601b5e877114
dnavaria <dk17816@gmail.com>
2022-03-01 13:29:37 +0530

    practise

--- python/test.py
+++ python/test.py
@@ -2,9 +2,17 @@ from os import read,fstat
 from io import BytesIO
 
 def fio():
-    return BytesIO(read(0,fstat(0).st_size)).readline().decode()
+	return BytesIO(read(0,fstat(0).st_size)).readline().decode()
 
 def sol():
-    pass
+	pass
 
-sol()
+str=input().split()
+n,m=(int(str[0]),int(str[1]))
+li=[[int(j) for j in input().split()] for i in range(n)]
+ridx = 0
+for i in range(n,0,-1):
+	for j in range(0,i):
+		print(*li[ridx])
+	ridx+=1
+
